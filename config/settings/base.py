65db43227365c9b9aa88849037c0a5a38bbbe9e7
hasithsen <sen.hasith@gmail.com>
2024-05-19 07:06:31 +0000

    feat: Create userprofiles app

--- config/settings/base.py
+++ config/settings/base.py
@@ -82,6 +82,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "biopage.users",
     # Your stuff: custom apps go here
+    "biopage.userprofiles",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
