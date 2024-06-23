81179e7b2240809fe1e8906e19056c82462f24a8
hasithsen <sen.hasith@gmail.com>
2024-06-23 08:25:41 +0000

    feat: Redirect to userprofile create page if no userprofile is associated with logged in user

--- biopage/users/views.py
+++ biopage/users/views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import redirect
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import DetailView
 from django.views.generic import RedirectView
 from django.views.generic import UpdateView
 
+from biopage.userprofiles.models import UserProfile
 from biopage.users.models import User
 
 
@@ -13,6 +15,14 @@ class UserDetailView(LoginRequiredMixin, DetailView):
     model = User
     slug_field = "username"
     slug_url_kwarg = "username"
+
+    def dispatch(self, *args, **kwargs):
+        if not UserProfile.objects.filter(user=self.request.user).exists():
+            # Redirect to profile detail page or any other page
+            return redirect(
+                "userprofiles:create",
+            )
+        return super().dispatch(*args, **kwargs)
 
 
 user_detail_view = UserDetailView.as_view()
